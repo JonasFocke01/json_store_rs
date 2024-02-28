1a1e6ed78230617e1c60cb546a37f5dbc23eff9a
jonas <@DISTRIB_ID=Pop.com>
2024-02-28 14:40:31 +0100

    `JsonStore` is now a trait

--- src/lib.rs
+++ src/lib.rs
@@ -31,22 +31,11 @@ impl std::error::Error for JsonStoreError {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct JsonStore<T> {
-    data: T,
-    #[serde(skip)]
-    db_file_path: PathBuf,
-    #[serde(skip)]
-    unsaved_writes: usize,
-}
-
-impl<T> JsonStore<T>
-where
-    T: Serialize + DeserializeOwned + Default + Clone,
-{
-    pub fn load(db_file_path: PathBuf) -> Result<Self, JsonStoreError> {
-        match Self::read_and_deserialize_file(db_file_path.clone()) {
-            Err(JsonStoreError::FilecontentNotValid) => match Self::backup_db_file(db_file_path) {
+pub trait JsonStore: Serialize + DeserializeOwned + Default + Clone {
+    fn db_file_path() -> PathBuf;
+    fn load() -> Result<Self, JsonStoreError> {
+        match Self::read_and_deserialize_file() {
+            Err(JsonStoreError::FilecontentNotValid) => match Self::backup_db_file() {
                 Ok(_) => Err(JsonStoreError::FilecontentNotValid_CreatedBackupfile),
                 Err(_) => Err(JsonStoreError::FilecontentNotValid_CouldNotCreateBackupfile),
             },
@@ -54,57 +43,29 @@ where
             Ok(s) => Ok(s),
         }
     }
-    pub fn setup(db_file_path: PathBuf) -> Result<Self> {
-        let json_store = Self {
-            data: T::default(),
-            db_file_path: db_file_path.clone(),
-            unsaved_writes: 0,
-        };
+    fn setup() -> Result<Self> {
+        let t = Self::default();
+        t.serialize_and_write_file()?;
 
-        json_store.serialize_and_write_file()?;
-
-        Ok(json_store)
+        Ok(t)
     }
 
     // TODO: migrate funtion
 
-    pub fn data(&self) -> &T {
-        &self.data
-    }
-    pub fn data_mut(&mut self) -> &mut T {
-        self.unsaved_writes = self.unsaved_writes.saturating_add(1);
-        &mut self.data
-    }
-    pub fn update(&mut self, update_fn: fn(T) -> T) -> Result<()> {
-        self.data = update_fn(self.data.clone());
+    fn write(&mut self) -> Result<bool> {
         self.serialize_and_write_file()?;
-        self.unsaved_writes = 0;
-        Ok(())
+        Ok(true)
     }
 
-    pub fn write(&mut self) -> Result<bool> {
-        if self.unsaved_writes > 0 {
-            self.serialize_and_write_file()?;
-            self.unsaved_writes = 0;
-            return Ok(true);
-        }
-        Ok(false)
-    }
-}
-
-impl<T> JsonStore<T>
-where
-    T: Serialize + DeserializeOwned,
-{
-    fn backup_db_file(db_file_path: PathBuf) -> Result<u64> {
-        let mut backup_file_path = db_file_path.clone();
+    fn backup_db_file() -> Result<u64> {
+        let mut backup_file_path = Self::db_file_path().clone();
         backup_file_path.pop();
         backup_file_path.push("backup.json");
-        Ok(std::fs::copy(db_file_path.clone(), backup_file_path).unwrap())
+        Ok(std::fs::copy(Self::db_file_path().clone(), backup_file_path).unwrap())
     }
     fn serialize_and_write_file(&self) -> Result<()> {
         let serialized_data = serde_json::to_string_pretty(&self)?;
-        let Ok(path_as_string) = self.db_file_path.clone().into_os_string().into_string() else {
+        let Ok(path_as_string) = Self::db_file_path().clone().into_os_string().into_string() else {
             return Err(JsonStoreError::PathNotValid.into());
         };
 
@@ -120,8 +81,9 @@ where
         Ok(())
     }
 
-    fn read_and_deserialize_file(db_file_path: PathBuf) -> Result<Self, JsonStoreError> {
-        let Ok(parsed_db_file_path) = db_file_path.clone().into_os_string().into_string() else {
+    fn read_and_deserialize_file() -> Result<Self, JsonStoreError> {
+        let Ok(parsed_db_file_path) = Self::db_file_path().clone().into_os_string().into_string()
+        else {
             return Err(JsonStoreError::PathNotValid);
         };
         let Ok(data_str) = std::fs::read_to_string(parsed_db_file_path.as_str()) else {
@@ -130,10 +92,6 @@ where
         let Ok(data) = serde_json::from_str(&data_str) else {
             return Err(JsonStoreError::FilecontentNotValid);
         };
-        Ok(Self {
-            data,
-            db_file_path,
-            unsaved_writes: 0,
-        })
+        Ok(data)
     }
 }
